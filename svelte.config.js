70458799163751c5313986efbb332323850c231a
AguzzTN54 <gusagusnanang@gmail.com>
2022-04-28 12:57:25 +0700

    Setup Tailwindcss

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: preprocess({
+		postcss: {
+			plugins: [tailwindcss, autoprefixer]
+		}
+	}),
 	kit: {
 		prerender: {
 			default: true
---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		fontFamily: {
+			riangriung: 'riangriung',
+			prompt: 'prompt'
+		},
+		extend: {}
+	},
+	plugins: []
+};
